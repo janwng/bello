bc3c517fa204a9f5861fb9b16ac8ef2332e91528
Jan Wong <janwong94@gmail.com>
2017-05-12 14:27:42 -0700

    finish setting up chat server

--- server/index.js
+++ server/index.js
@@ -10,9 +10,17 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user has connected!');
 
+  // when socket receives a chat message
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    // send the message to everyone
+    io.emit('chat message', msg);
+  });
+
+  // when a socket gets disconnected
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  })
+  });
 });
 
 http.listen(3000, function(){
